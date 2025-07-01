19f43bd9278c99319560dcedf1d122aca7bd568f
Priyanka <piya3966@gmail.com>
2025-07-01 17:18:43 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "hotstar-bucket-1-07-2025" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-southeast-1"
   }
 }
